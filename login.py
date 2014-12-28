174432c582a968fddd81ea5430ca3a8a29ba7bf5
Deanna Taylor <dmtaylorny@gmail.com>
2014-12-28 13:25:25 -0500

    line

--- login.py
+++ login.py
@@ -1,14 +1,15 @@
 import pymongo, csv
 from pymongo import Connection
 
-
 conn = Connection()
 db = conn["rulo"]
 people = db.people
 events = db.events
 
-
 def newUser(dict):
     people.insert(dict)
 
 # dict should have name, username, etc 
+
+def newEvent(ev):
+    events.insert(ev)
