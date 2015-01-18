7f0ed03e0ae95af800fdf33e9fb75607fa4a21c0
Sophie Gershon <sophgersh@gmail.com>
2015-01-18 12:00:35 -0500

    please work

--- util.py
+++ util.py
@@ -108,19 +108,13 @@ if __name__ == "__main__":
     #for person in users.find():
     #    users.remove(person)
     print listEvents()
-<<<<<<< HEAD
     
-    addPersonEvent('ssss','ObjectId(54b879a767a8a20cff85754c)')
-    for e in events.find():
-        print e['_id']
-    
-=======
     '''
     addPersonEvent('ssss','ObjectId(54b879a767a8a20cff85754c)')
     for e in events.find():
         print e['_id']
     '''
->>>>>>> master
+
 
     
 
