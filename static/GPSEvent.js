1b2f50df3173c1b671f641c9ba14021549ba511e
Benedict-Bolton <benedict.bolton@gmail.com>
2015-01-23 12:34:07 -0500

    adding distance

--- static/GPSEvent.js
+++ static/GPSEvent.js
@@ -67,6 +67,39 @@ var action = function () {
     console.log('locating complete.');
     console.log("button action complete");
 }
+
+var disctance = function (origin, destination){
+    var service = google.maps.DistanceMatrixService();
+
+    service.getDistanceMatrix(
+	{
+	    origins: origin,
+	    destinations: destination,
+	    unitSystem: google.maps.UnitSystem.IMPERIAL
+	}, callback);
+    
+    function callback (response, status) {
+	if (status == google.maps.GeocoderStatus.OK){
+	    var origins = response.originAddress;
+	    var destinations = response.destinationAddresses;
+	    
+	    for (var i = 0; i < origins.length; i++){
+		var results = response.rows[i].elements;
+		for (var j = 0; j < results.length; j++){
+		    var element = results[j];
+		    var distance = element.distance.text;
+		    var duration = element.duration.text;
+		    var from = origins[i];
+		    var to = origins[i];
+		}
+	    }
+	}
+	else {
+	    alert("Distance Calculation failed due to: " + status);
+	}
+    }
+}
+
 /*var showPosition = function(position) {
     var lat = document.getElementById("lat");
     var long = document.getElementById("long");
